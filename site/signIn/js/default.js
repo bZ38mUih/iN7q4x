af282c4a5d3d0eaff7f4a70bd4accd5fed72d255
Right Joint <rightjoint@yandex.ru>
2020-04-08 20:05:06 +0300

    ref-pr-1

--- site/signIn/js/default.js
+++ site/signIn/js/default.js
@@ -1,3 +1,28 @@
+$(document).ready(function() {
+    $("form.signIn input[name=login]").keyup(function () {
+        if ($(this).val() != "") {
+            $("label[for='login']").removeClass("shown");
+        } else {
+            $("label[for='login']").addClass("shown");
+        }
+    });
+    $("form.signIn input[name=password]").keyup(function () {
+        if ($(this).val() != "") {
+            $("label[for='password']").removeClass("shown");
+        } else {
+            $("label[for='password']").addClass("shown");
+        }
+    });
+    $("form.signIn input[name=rememberMe]").click(function(){
+        if($(this).prop('checked') == true){
+            $.cookie('rememberMe', "on", {expires: 30, path:'/'});
+        }else{
+            $.cookie('rememberMe', null, {expires: 30, path:'/'});
+            $.cookie('password', null, {expires: 30, path:'/'});
+        };
+    });
+});
+
 function showGate(attrId)
 {
     if($("#"+attrId).hasClass('active')===false){
---
+++ site/siteMan/defaultController.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mrSmitch
+ * Date: 08.04.2020
+ * Time: 18:26
+ */
