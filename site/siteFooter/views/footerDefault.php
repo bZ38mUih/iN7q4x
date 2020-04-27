73fe0f4d27deaa5b7cc6078dc054d57359403a84
bZ38mUih <alenanice37@gmail.com>
2020-04-27 22:43:47 +0300

    metrika

--- site/siteFooter/views/footerDefault.php
+++ site/siteFooter/views/footerDefault.php
@@ -1,18 +1,18 @@
 <?php
 $appRJ->response['result'].= "<div class='contentBlock-frame dark ft'><div class='contentBlock-center'>".
     "<div class='contentBlock-wrap'><footer>";
-/*
+
 if($App['views']['social-block']) {
     $appRJ->response['result'].= "<div class='ft-service'>".
         "<noscript><div>".
-        "<img src='https://mc.yandex.ru/watch/44136454' style='position:absolute; left:-9999px;' alt='' /></div></noscript>".
-        "<a href='https://metrika.yandex.ru/stat/?id=44136454&amp;from=informer' target='_blank' rel='nofollow'>".
-        "<img src='https://informer.yandex.ru/informer/44136454/3_1_FFFFFFFF_EFEFEFFF_0_pageviews' ".
+        "<img src='https://mc.yandex.ru/watch/62372254' style='position:absolute; left:-9999px;' alt='' /></div></noscript>".
+        "<a href='https://metrika.yandex.ru/stat/?id=62372254&amp;from=informer' target='_blank' rel='nofollow'>".
+        "<img src='https://informer.yandex.ru/informer/62372254/3_1_FFFFFFFF_EFEFEFFF_0_pageviews' ".
         "style='width:88px; height:31px; border:0;' alt='Яндекс.Метрика' title='Яндекс.Метрика: данные за сегодня (просмотры, ".
-        "визиты и уникальные посетители)' class='ym-advanced-informer' data-cid='44136454' data-lang='ru' /></a>".
+        "визиты и уникальные посетители)' class='ym-advanced-informer' data-cid='62372254' data-lang='ru' /></a>".
         "</div>";
 }
-*/
+
 $appRJ->response['result'].= "<div class='ft-center'>".
 
     //"<div class='ft-main'>".
